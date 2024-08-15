17eb18ea58afd5612df624ad6d0168af120d18ae
VinyciusLupi <165966679+VinyciusLupi@users.noreply.github.com>
2024-08-14 21:35:49 -0300

    Add files via upload

---
+++ aula03/Program.cs
@@ -0,0 +1,58 @@
+﻿//int numero = 10;
+//double pi = 3.1415;
+//char letra = 'A';
+//bool isTrue = true;
+//string mensagem = "Olá mundo!";
+
+//int x;
+//Console.WriteLine("Digitar o valor de x: ");
+//x = Convert.ToInt32 (Console.ReadLine());
+//usando concatentação
+//Console.WriteLine("O usuario digitou: " + x + " parabens");
+//interpolação de strings
+//Console.WriteLine($"O usuario digitou: {x} parabems");
+//placeholders
+//Console.WriteLine("O usuario digitou: {0} parabens", x);
+
+int num1, num2;
+Console.WriteLine("digite um numero: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("digite o segundo numero!");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+int soma;
+soma = num1 + num2;
+
+int sub;
+sub = num1 - num2;
+
+int mult;
+mult = num1 * num2;
+
+double div;
+div = num1 / num2;
+
+
+
+Console.WriteLine("a soma de " + num1 + " + " + num2+ " é : " + soma +".");
+
+Console.WriteLine("a subtração de {0} - {1} é: {2}", num1, num2, sub);
+
+Console.WriteLine($"a multiplicação de {num1} X {num2} é: {mult}");
+
+Console.WriteLine($"a divisão de {num1} / {num2} é: {div}");
+
+
+
+//double outronumero;
+
+//outronumero = numero;
+
+//nt terraDeareia = (int) pi;
+
+//Console.WriteLine(pi);
+//Console.WriteLine(terraDeareia);
+
+//int numero2 = 5;
+
+//string texto = Convert.ToString(numero2);,
